7f3fa5ea298dd20a7399e08449e66263639df822
Chris McDonough <chrism@plope.com>
2022-07-03 09:44:37 -0400

    add p50 and p52

---
+++ lenovo/thinkpad/p50/default.nix
@@ -0,0 +1,34 @@
+{ lib, ... }:
+{
+  imports = [
+    ../../../common/gpu/nvidia.nix
+    ../../../common/cpu/intel
+    ../../../common/pc/laptop/acpi_call.nix
+    ../.
+  ];
+
+  hardware = {
+    nvidia = {
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+
+    opengl = {
+      driSupport = lib.mkDefault true;
+      driSupport32Bit = lib.mkDefault true;
+    };
+  };
+
+  # NB: the p53 profile uses throttled to prevent too-eager CPU
+  # throttling instead of thermald.  I understand throttled to have been a
+  # workaround solution at the time that profile was created.  Thermald would
+  # have been preferred if it worked at the time. I read
+  # https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues
+  # as saying that throttled is no longer necessary given version 5.12+ of the
+  # kernel combined with version 2.4.3+ of thermald.  At the time of this
+  # writing, the stable NixOS kernel is 5.15 and 2.4.9 of thermald.  So we use
+  # thermald.
+  services.thermald.enable = lib.mkDefault true;
+}
