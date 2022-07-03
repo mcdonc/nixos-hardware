fa7affa7d69981a09c8e092615236c408b368417
Chris McDonough <chrism@plope.com>
2022-07-03 00:55:11 -0400

    remove unnecessary and too-eager directives

--- lenovo/thinkpad/p51/default.nix
+++ lenovo/thinkpad/p51/default.nix
@@ -10,20 +10,16 @@
   hardware = {
     nvidia = {
       prime = {
-        # offload.enable = true; # enable to use intel gpu (hybrid mode)
-        sync.enable = true; # enable to use nvidia gpu (discrete mode)
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
-      modesetting.enable = false;
-      powerManagement.enable = true;
+      # marked as "experimental" in the source
+      # powerManagement.enable = true;
     };
 
-    # other opengl stuff is included via <nixos-hardware/common/cpu/intel> (including 
-    # intel-media-driver and vaapiIntel)
+    # other opengl stuff is included via <nixos-hardware/common/cpu/intel> and
+    # <nixos0hardware/common/gpu/nvidia.nix>
     opengl = {
-      enable = true;
-      extraPackages = with pkgs; [ vaapiVdpau ];
       driSupport = true;
       driSupport32Bit = true;
     };
