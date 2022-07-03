73fae87b9f3dd5213c0c0712519db17a16a5ccb5
Chris McDonough <chrism@plope.com>
2022-07-03 01:07:19 -0400

    comment and mkDefault

--- lenovo/thinkpad/p51/default.nix
+++ lenovo/thinkpad/p51/default.nix
@@ -13,17 +13,20 @@
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
-      # marked as "experimental" in the source
-      # powerManagement.enable = true;
     };
 
     # other opengl stuff is included via <nixos-hardware/common/cpu/intel> and
-    # <nixos0hardware/common/gpu/nvidia.nix>
+    # <nixos-hardware/common/gpu/nvidia.nix>.  I'm not sure enabling DRI here
+    # is too aggressive.  Maybe try without and see what happens to steam and
+    # accelerated video in Chromium/FF.
     opengl = {
-      driSupport = true;
-      driSupport32Bit = true;
+      driSupport = lib.mkDefault true;
+      driSupport32Bit = lib.mkDefault true;
     };
   };
-  
+
+  # note that the p53 profile uses throttled instead; I read
+  # https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues
+  # as saying that this is no longer necessary.
   services.thermald.enable = lib.mkDefault true;
 }
